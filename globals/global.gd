34f5985817b0b02d93027fedd756419d2958a558
Jodios <35905299+Jodios@users.noreply.github.com>
2025-08-24 07:08:05 -0500

    just adding basic transition

--- globals/global.gd
+++ globals/global.gd
@@ -3,6 +3,10 @@ extends Node
 # For dev purposes
 # TODO: SET ALL TO FALSE BEFORE SUBMISSION
 const IS_DEV: bool = true
-const SKIP_MAIN_MENU: bool = true
+const SKIP_MAIN_MENU: bool = false
 
 # others
+var viewport_size: Vector2
+
+func _ready() -> void:
+    viewport_size = get_viewport().get_visible_rect().size
