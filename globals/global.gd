1351fd274310f08582894a86bd6211d6b67c4a90
Jodios <35905299+Jodios@users.noreply.github.com>
2025-08-24 07:09:05 -0500

    forgot to disable start screen

--- globals/global.gd
+++ globals/global.gd
@@ -3,7 +3,7 @@ extends Node
 # For dev purposes
 # TODO: SET ALL TO FALSE BEFORE SUBMISSION
 const IS_DEV: bool = true
-const SKIP_MAIN_MENU: bool = false
+const SKIP_MAIN_MENU: bool = true
 
 # others
 var viewport_size: Vector2
